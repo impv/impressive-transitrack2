9b161e0ffabb96f3e15d0e0e2a47bda70fa6ca31
Misato <rexlove1226@gmail.com>
2025-12-16 11:29:40 +0900

    chore: プッシュしないファイルを整理

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
 	return (
 		<SessionProvider session={session}>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
-}
+};
+
+export default App;
