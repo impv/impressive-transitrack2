e368539a649e428c338884c2d85e070c388a614e
Misato <rexlove1226@gmail.com>
2026-02-17 16:27:42 +0900

    refactor: コードをリファクタリング

--- src/types/expenses.ts
+++ src/types/expenses.ts
@@ -1,12 +1,37 @@
 import type { TransportType, TripType } from "@prisma/client";
 
 export interface Expense {
+  /**
+   * メンバーのID
+   */
   memberId: string;
+  /**
+   * 日付
+   */
   date: string;
+  /**
+   * 内容
+   */
   departure: string;
+  /**
+   * 金額
+   */
   arrival: string;
+  /**
+   * 金額
+   */
   amount: number;
+  /**
+   * 交通手段
+   * TRAIN: 電車
+   * BUS: バス
+   */
   transport: TransportType;
+  /**
+   * 片道、往復
+   * ONEWAY: 片道
+   * ROUNDTRIP: 往復
+   */
   tripType: TripType;
 }
 
